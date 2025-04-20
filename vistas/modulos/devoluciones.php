11b5e6b2c3333b3ffe7aaaeca7af8d74fd1d74a8
Jhon Diaz <djhon8366@gmail.com>
2025-04-20 16:19:12 -0500

    se enruta a la vista devoluciones y se creo la tabla con datos quemados

--- vistas/modulos/devoluciones.php
+++ vistas/modulos/devoluciones.php
@@ -1,15 +1,16 @@
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Devoluciones</h1>
+            <h1>devoluciones</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">inicio</li>
             </ol>
           </div>
         </div>
@@ -18,12 +19,59 @@
 
     <!-- Main content -->
     <section class="content">
-
-      <!-- Default box -->
-
-      <!-- /.card -->
-
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <table id="tblSedes" class="table table-bordered table-striped">
+                  <thead>
+                    <tr>
+                      <th>identificacion</th>
+                      <th>Nombre/rol</th>
+                      <th>ficha</th>
+                      <th>fecha</th>
+                      <th>placa</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>123456</td>
+                      <td>Juan Perez / Admin</td>
+                      <td>F123</td>
+                      <td>2023-10-01</td>
+                      <td>ABC123</td>
+                    </tr>
+                    <tr>
+                      <td>789012</td>
+                      <td>Maria Lopez / User</td>
+                      <td>F456</td>
+                      <td>2023-10-02</td>
+                      <td>XYZ789</td>
+                    </tr>
+                    <tr>
+                      <td>345678</td>
+                      <td>Carlos Ruiz / Manager</td>
+                      <td>F789</td>
+                      <td>2023-10-03</td>
+                      <td>LMN456</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div>
     </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    <div class="card-footer d-flex justify-content-end ml-2">
+      <button  class="btn btn-danger float-right mr-2 ml-2" data-toggle="modal" data-target="#modalMalEstado">
+        Mal Estado
+      </button>
+      <button  class="btn btn-success float-right" data-toggle="modal" data-target="#modalBuenEstado" >
+        Buen Estado
+      </button>
+    </div>
+</div>
